2ead30bbc0c82234b559b0a41a522346ee5e3164
Rahul Rudra <rudra.rahul71@gmail.com>
2025-11-07 12:10:57 -0500

    responsive header

--- lib/core/page_header.dart
+++ lib/core/page_header.dart
@@ -21,31 +21,36 @@ class _PageHeaderState extends State<PageHeader> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.header,
-                style: const TextStyle(
-                  fontSize: 28,
+      child: SizedBox(
+        width: double.infinity,
+        child: Wrap(
+          alignment: WrapAlignment.spaceBetween,
+          runSpacing: 20.0,
+          spacing: 20.0,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.header,
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                widget.sub,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  widget.sub,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(context).colorScheme.surfaceBright,
+                  ),
                 ),
-              ),
-            ],
-          ),
-              
-          ?widget.action
-        ],
-      ),
+              ],
+            ),
+            ?widget.action,
+          ],
+        ),
+      )
     );
   }
 }
