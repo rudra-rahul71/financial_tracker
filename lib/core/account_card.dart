be6eace3a878362b21777c954ec9342a777c6ce1
Rahul Rudra <rudra.rahul71@gmail.com>
2025-11-11 15:55:55 -0500

    finalize account card

--- lib/core/account_card.dart
+++ lib/core/account_card.dart
@@ -14,7 +14,9 @@ class AccountCard extends StatefulWidget {
 }
 
 class _AccountCardState extends State<AccountCard> {
-  late Account _account = widget.connection.value.$2[0];
+  late final _totalValue = widget.connection.value.$2.fold(
+    0.0, (double previousSum, Account account) => previousSum + (account.available ?? 0.0),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +56,7 @@ class _AccountCardState extends State<AccountCard> {
                               ),
                             ),
                             Text(
-                              _account.name,
+                              '${widget.connection.value.$2.length} accounts',
                               style: TextStyle(
                                 fontSize: 14,
                                 color: Theme.of(context).colorScheme.inversePrimary,
@@ -71,14 +73,14 @@ class _AccountCardState extends State<AccountCard> {
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     const Text(
-                      'Balance',
+                      'Total Balance',
                       style: TextStyle(
                         fontSize: 14,
                         color: Colors.grey,
                       ),
                     ),
                     Text(
-                      '\$${_account.available!.toStringAsFixed(2)}',
+                      '\$${_totalValue.toStringAsFixed(2)}',
                       style: const TextStyle(
                         fontSize: 28,
                         fontWeight: FontWeight.bold,
@@ -89,53 +91,36 @@ class _AccountCardState extends State<AccountCard> {
               ],
             ),
             const SizedBox(height: 12.0),
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  height: 40,
-                  width: 140,
-                  child: DropdownButtonFormField(
-                    initialValue: widget.connection.value.$2[0],
-                    dropdownColor: Theme.of(context).colorScheme.onPrimary,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Theme.of(context).colorScheme.onPrimary, 
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                    items: widget.connection.value.$2.map<DropdownMenuItem<Account>>((Account value) {
-                      return DropdownMenuItem<Account>(
-                        value: value,
-                        child: Text(value.subetype),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        _account = value!;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
             const Divider(height: 24.0),
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[300]!, width: 1.0),
-                borderRadius: BorderRadius.circular(8.0),
-                color: Colors.grey[50],
-              ),
-              alignment: Alignment.center,
-              child: const Text(
-                'Chart Placeholder\n(Use a package like fl_chart or charts_flutter here)',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.grey, fontStyle: FontStyle.italic),
-              ),
-            ),
+            ...widget.connection.value.$2.map((account) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 7),
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(account.name),
+                      Text(account.subetype,
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text('Current: \$${account.current!.toStringAsFixed(2)}'),
+                      Text('Available: \$${account.available!.toStringAsFixed(2)}')
+                    ],
+                  ),
+                ],
+                ),
+              );
+            }),
           ],
         ),
       ),
